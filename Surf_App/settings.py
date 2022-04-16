bca7f37a913d142bf8765267b2972f64c38ec8d6
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-04-15 22:28:15 -0400

    Add files via upload

--- Surf_App/settings.py
+++ Surf_App/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'surf_report.apps.SurfReportConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -116,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
--- Surf_App/urls.py
+++ Surf_App/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from surf_report import views
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+from users.models import CustomAuthForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('register', user_views.register, name='register'),
+    path('favorites', user_views.favorites, name='favorites'),
+    path('login', auth_views.LoginView.as_view(authentication_form=CustomAuthForm, template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
